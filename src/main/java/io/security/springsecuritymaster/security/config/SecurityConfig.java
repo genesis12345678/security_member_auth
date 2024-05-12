5521dd548a979f0f2f9f7f5f6b1a924c4c08765c
장근영 <jky00914@naver.com>
2024-05-12 13:33:33 +0900

    프로그래밍 방식 인가 구현(메모리 기반)

--- src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import io.security.springsecuritymaster.security.handler.FormAuthenticationSucce
 import io.security.springsecuritymaster.security.handler.RestAccessDeniedHandler;
 import io.security.springsecuritymaster.security.handler.RestAuthenticationFailureHandler;
 import io.security.springsecuritymaster.security.handler.RestAuthenticationSuccessHandler;
+import io.security.springsecuritymaster.security.manager.CustomDynamicAuthorizationManager;
 import io.security.springsecuritymaster.security.provider.FormAuthenticationProvider;
 import io.security.springsecuritymaster.security.provider.RestAuthenticationProvider;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,10 +18,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 @Configuration
@@ -30,25 +33,18 @@ public class SecurityConfig {
 
     private final FormAuthenticationProvider formAuthenticationProvider;
     private final RestAuthenticationProvider restAuthenticationProvider;
-
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
-
     private final FormAuthenticationSuccessHandler formSuccessHandler;
     private final FormAuthenticationFailureHandler formFailureHandler;
-
     private final RestAuthenticationSuccessHandler restSuccessHandler;
     private final RestAuthenticationFailureHandler restFailureHandler;
+    private final AuthorizationManager<RequestAuthorizationContext> authorizationManager;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**", "/favicon.*", "/*/icon-*").permitAll() //정적 자원 관리
-                        .requestMatchers("/", "/signup", "/login*").permitAll()
-                        .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/manager").hasRole("MANAGER")
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+                        .anyRequest().access(authorizationManager))
                 .formLogin(form -> form
                         .loginPage("/login").permitAll() //커스텀 로그인 페이지
                         .authenticationDetailsSource(authenticationDetailsSource)
---
+++ src/main/java/io/security/springsecuritymaster/security/manager/CustomDynamicAuthorizationManager.java
@@ -0,0 +1,78 @@
+package io.security.springsecuritymaster.security.manager;
+
+import io.security.springsecuritymaster.security.mapper.MapBasedUrlRoleMapper;
+import io.security.springsecuritymaster.security.service.DynamicAuthorizationService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.log.LogMessage;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+@Component
+@RequiredArgsConstructor
+public class CustomDynamicAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+
+    private List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings;
+    private static final AuthorizationDecision DENY = new AuthorizationDecision(false);
+    private final HandlerMappingIntrospector handlerMappingIntrospector;
+
+    @PostConstruct
+    public void mapping() {
+        DynamicAuthorizationService das = new DynamicAuthorizationService(new MapBasedUrlRoleMapper());
+        mappings = das.getUrlRoleMappings()
+                      .entrySet()
+                      .stream()
+                      .map(entry -> new RequestMatcherEntry<>(
+                              new MvcRequestMatcher(handlerMappingIntrospector, entry.getKey()),
+                              customAuthorizationManager(entry.getValue())
+                      ))
+                      .toList();
+    }
+
+    private AuthorizationManager<RequestAuthorizationContext> customAuthorizationManager(String role) {
+        if (role != null) {
+            if (role.startsWith("ROLE")) {
+                return AuthorityAuthorizationManager.hasAuthority(role);
+            } else {
+                return new WebExpressionAuthorizationManager(role);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * RequestMatcherDelegatingAuthorizationManager 클래스에 check() 메서드 그대로
+     */
+    @Override
+    public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext request) {
+        for (RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> mapping : this.mappings) {
+
+            RequestMatcher matcher = mapping.getRequestMatcher();
+            RequestMatcher.MatchResult matchResult = matcher.matcher(request.getRequest());
+
+            if (matchResult.isMatch()) {
+                AuthorizationManager<RequestAuthorizationContext> manager = mapping.getEntry();
+                return manager.check(authentication,
+                        new RequestAuthorizationContext(request.getRequest(), matchResult.getVariables()));
+            }
+        }
+        return DENY;
+    }
+
+    @Override
+    public void verify(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        AuthorizationManager.super.verify(authentication, object);
+    }
+}
